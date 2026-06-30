d8c3eec4cabe3c8c004746b979185b1746ace558
zmzlois <lois.z@comcord.vision>
2026-06-30 11:33:33 +0100

    Update value.cpp

--- src/value.cpp
+++ src/value.cpp
@@ -1,4 +1,7 @@
 #include "value.hpp"
+#include <iostream>
+#include <unordered_set>
+#include <vector>
 
 Value::Value(double data_value, std::vector<ValuePtr> children,
              std::string operation)
@@ -8,7 +11,31 @@ Value::Value(double data_value, std::vector<ValuePtr> children,
 {
   previous.reserve(children.size());
 
-  for (const auto &child : children) {
+  for (const auto& child : children) {
     previous.push_back(child);
+  }
+}
+
+void Value::backward() {
+  std::vector<ValuePtr> topo;
+  std::unordered_set<Value*> visited;
+
+  build_topology(shared_from_this(), topo, visited);
+
+  grad = 1.0;
+
+  for (auto it = topo.rbegin(); it != topo.rend(); ++it) {
+    (*it) -> backward_fn();
+  }
+}
+
+void Value::print() const {
+  std::cout << "Value - data: " << data << ", grad: " << grad << "\n";
+}
+
+void Value::build_topology(const ValuePtr &node, std::vector<ValuePtr> &topo,
+                           std::unordered_set<Value*>& visited) {
+  if (visited.find(node.get()) != visited.end()) {
+    return;
   }
 }
