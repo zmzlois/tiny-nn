9cb3f3f54aa6131ba582c7b7b1c5963b27eadfec
zmzlois <lois.z@comcord.vision>
2026-07-01 12:01:49 +0100

    add

--- src/value.cpp
+++ src/value.cpp
@@ -1,5 +1,6 @@
 #include "value.hpp"
 #include <iostream>
+#include <memory>
 #include <unordered_set>
 #include <vector>
 
@@ -9,6 +10,7 @@ Value::Value(double data_value, std::vector<ValuePtr> children,
       backward_fn([]() {})
 
 {
+  // increase the capacity of `previous` value if children's size is larger than previous value, otherwise do nothing
   previous.reserve(children.size());
 
   for (const auto& child : children) {
@@ -41,9 +43,33 @@ void Value::build_topology(const ValuePtr& node, std::vector<ValuePtr>& topo,
 
   visited.insert(node.get());
 
+  // don't allow modifying origianl value and don't make copy of it, for each weak pointer to a previous weak node, borrow it read-only as `weak_child`
   for (const auto &weak_child : node->previous) {
+    // try to turn this weak pointer into a usable shared pointer. if the object still exists, call it `child` and enter the block
     if (auto child = weak_child.lock()) {
       build_topology(child, topo,visited);
     }
   }
+
+  topo.push_back(node);
+}
+
+ValuePtr value(double data) {
+  return std::make_shared<Value>(data);
+}
+
+ValuePtr add(const ValuePtr &left, const ValuePtr &right) {
+  auto out = std::make_shared<Value>(left->data + right->data,
+                                     std::vector<ValuePtr>{left, right}, "+");
+
+  std::weak_ptr<Value> out_weak = out;
+
+  out->backward_fn = [left, right, out_weak]() {
+    if (auto out = out_weak.lock()) {
+      left->grad += out->grad;
+      right->grad += out->grad;
+    }
+  };
+
+  return out;
 }
